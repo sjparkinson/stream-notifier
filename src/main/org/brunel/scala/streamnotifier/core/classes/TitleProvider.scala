ed05c96faff15e30bef2c2672fa2f31fde209be2
ayushn21 <ayushnewatia@gmail.com>
2014-02-17 14:47:35 +0000

    Timestamp
    Added Timestamp to TitleProvider

--- src/main/org/brunel/scala/streamnotifier/core/classes/TitleProvider.scala
+++ src/main/org/brunel/scala/streamnotifier/core/classes/TitleProvider.scala
@@ -2,6 +2,6 @@ package main.org.brunel.scala.streamnotifier.core.classes
 
 import java.util.UUID
 import java.net.URL
+import java.sql.Timestamp
 
-
-case class TitleProvider(id: Option[UUID], title: Title, provider: Provider, url: URL)
+case class TitleProvider(id: Option[UUID], title: Title, provider: Provider, url: URL, expiry: Timestamp)
